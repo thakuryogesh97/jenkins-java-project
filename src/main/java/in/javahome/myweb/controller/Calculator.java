95243337609754b51c9fdf97979b05c7cb019a4e
thakuryogesh97 <pedagogy38@gmail.com>
2025-11-16 01:58:52 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,22 +1,8 @@
-package in.javahome.myweb.controller;
-/*
- * 
- */
-public class Calculator {
-	
-	/*
-	 * @param i
-	 * @param j
-	 * @return int
-	 */
-	public int add(int i, int j){
-		return i+j;
-	}
-	public int multiply(int i, int j){
-		return i*j;
-	}
-	
-	public int sub(int i, int j){
-		return i-j;
-	}
+package in.instagram.myweb.controller;
+
+public class InstagramController {
+
+    public String welcomeMessage() {
+        return "Welcome to Instagram Clone!";
+    }
 }
